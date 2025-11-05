e70c1ba9d6cf16a3cae42e44d42aaf8a70f4db6d
Darwin Rojas <josteinrojas590@gmail.com>
2025-11-04 19:17:27 -0500

    Probando la libreria pyttsx3

---
+++ prueba.py
@@ -0,0 +1,37 @@
+# Haciendo un pequeño proyecto para hacer que python puede leer texto
+# pip install pytsx3
+
+import pyttsx3
+
+def texto_a_voz(texto):
+    engine = pyttsx3.init()
+    engine.say(texto)
+    engine.runAndWait()
+
+def menu_opciones():
+    while True:
+        print("\n=== MENU DE OPCIONES ===")
+        print("1. Convertir texto a voz")
+        print("2. Conversar con el chat")
+        print("3. Salir")
+        
+        opcion = input("Elige una opcion ")
+        
+        if opcion == "1":
+            texto = input("Escribe un texto ")
+            texto_a_voz(texto)
+        elif opcion =="2":
+            texto_a_voz("Hola como estas en que te puedo ayudar")
+            texto2 = input("Responde al chatboot ") #Es un ejemplo
+            texto_a_voz(texto2)
+        elif opcion == "3":
+            break
+        else:
+            print("Esriba una opcion correcta ")
+
+# LLamamos al menu de opciones
+menu_opciones()    
+        
+    
+
+
